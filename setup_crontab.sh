c372fb443ddcdb32a1ce7aa5ba8121fefaac158c
azuelsdorf16 <azuelsdorf16@gmail.com>
2022-06-06 19:13:46 -0400

    updating crontab to restart server.

--- setup_crontab.sh
+++ setup_crontab.sh
@@ -5,9 +5,9 @@
 # Raspbian distribution with PiHole running on the same machine.
 
 if [ -d ~/domovoi ]; then
-        cd ~/domovoi && chmod +x ./run_domain_alert.sh && ./run_domain_alert.sh 2>&1 | tee ~/domovoi/run_domain_alert.sh.log
-        (crontab -l | grep -Fv "run_domain_alert.sh" | grep -Fv "start_interceptor.sh"; echo "*/5 * * * * cd ~/domovoi && sudo ./run_domain_alert.sh 2>&1 | tee ~/domovoi/run_domain_alert.sh.log") | crontab -
-        (crontab -l | grep -Fv "start_interceptor.sh"; echo "*/1 * * * * cd ~/domovoi && sudo ./start_interceptor.sh") | crontab -
+        (crontab -l | grep -Fv "run_domain_alert.sh" | grep -Fv "start_interceptor.sh" | grep -Fv "restart_server.sh"; echo "*/5 * * * * cd ~/domovoi && sudo ./run_domain_alert.sh 2>&1 | tee ~/domovoi/run_domain_alert.sh.log") | crontab -
+        (crontab -l | grep -Fv "start_interceptor.sh" | grep -Fv "restart_server.sh"; echo "*/1 * * * * cd ~/domovoi && sudo ./start_interceptor.sh") | crontab -
+        (crontab -l | grep -Fv "restart_server.sh"; echo "*/1 * * * * cd ~/domovoi && ./restart_server.sh") | crontab -
 else
         echo "Could not find directory 'domovoi' at home directory. Please clone domovoi to your home directory."
 fi
